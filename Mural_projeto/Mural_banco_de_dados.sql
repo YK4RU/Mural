8802fb29e792fd0202b3e8adee0727df6980f81f
YK4RU <168197162+YK4RU@users.noreply.github.com>
2025-05-14 20:16:39 -0300

    14/05

---
+++ Mural_projeto/Mural_banco_de_dados.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE mural;
+USE mural;
+
+create table usuario(
+	id_usuario serial unique,
+	email varchar(255) primary key,
+    nome varchar(255),
+    senha varchar(255) not null,
+    descricao varchar(255),
+    url_da_imagem_do_perfil varchar(500)
+);
+
+create table postagem(
+	id_postagem serial primary key,
+    foreign key (id_postagem) references usuario(id_usuario),
+    url_da_imagem varchar(500),
+	descricao varchar(255),
+	data_de_postagem timestamp default current_timestamp
+);
+
+create table pastas (
+	id_pasta serial primary key,
+    salvo int references postagem(postagem_id)
+);
+
+create table comentarios(
+	id_comentarios serial primary key,
+    id_postagem int references postagem(postagem_id),
+    id_usuario int references usuario(id_usuario),
+	conteudo varchar(255) not null
+);
+create table comentarios_dos_comentarios(
+	id_coment_dos_coment serial primary key,
+    id_postagem int references postagem(postagem_id),
+    id_comentario_pai int references comentarios(id_comentarios),
+	conteudo varchar(255) not null
+);
+
+create table likes(
+	id_likes serial primary key,
+    id_postagem int references postagem(postagem_id),
+    valor int
+);
+
+create table mural(
+	id_mural serial primary key,
+	url_das_conquistas varchar(500),
+	texto text
+);
---
+++ Mural_projeto/Müral/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mural</title>
+    <link rel="stylesheet" href="style_cadastro.css">
+</head>
+<body>
+    <form action="" method="post">
+        <label for="email"> email:</label>
+        <br>
+        <input type="email" id="email" name="email">
+        <br>
+        <label for="nome-usuario">nome de usuário;</label>
+        <br>
+        <input type="text" id="nome-usuario" name="nome-usuario">
+        <br>
+        <label for="senha">senha:</label>
+        <br>
+        <input type="password" id="senha" name="senha">
+        <br>
+        <input type="submit">
+    </form>
+
+</body>
+</html>
