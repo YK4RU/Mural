57864255b1b6fb547e7c1649787d54ebc4091d89
YK4RU <168197162+YK4RU@users.noreply.github.com>
2025-06-12 18:59:06 -0300

    feat: adicionado o cadastro

---
+++ Mural_projeto/Mural/Cadastro/config_cadastro.php
@@ -0,0 +1,34 @@
+<?php
+require_once("../conexao.php");
+session_start();
+
+if (isset($_POST["email"]) && isset($_POST["senha"]) && isset($_POST["nome_de_usuario"])) {
+    $email = trim($_POST["email"]);
+    $senha = trim($_POST["senha"]);
+    $nome_de_usuario = trim($_POST["nome_de_usuario"]);
+
+    if (empty($email) || empty($senha) || empty($nome_de_usuario)) {
+        echo "Preencha todos os campos";
+    }
+    else {
+        $sql_code = $conexao->prepare("INSERT INTO usuario (email, senha, nome_de_usuario) VALUES (:email, :senha, :nome_usuario )");
+        $sql_code->bindValue(':email', $email);
+        $sql_code->bindValue(':senha', $senha);
+        $sql_code->bindValue(':nome_usuario', $nome_de_usuario);
+        $sql_code->execute();
+
+        if ($sql_code->rowCount() > 0) {
+            $sql_select = $conexao->prepare("SELECT * FROM usuario WHERE email = :email");
+            $sql_select->bindValue(':email', $email);
+            $sql_select->execute();
+            $usuario = $sql_select->fetch();
+
+            $_SESSION['email'] = $usuario['email'];
+            $_SESSION['nome_de_usuario'] = $usuario['nome_de_usuario'];
+            header("location: ../home_page.php");
+            exit;
+        }
+    
+    } 
+}
+?>
