5a7a110d50a3b2bca5609fd8d058393214c80b4f
YK4RU <168197162+YK4RU@users.noreply.github.com>
2025-06-17 09:07:07 -0300

    Add files via upload

--- Mural_projeto/Mural/home_page.php
+++ Mural_projeto/Mural/home_page.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('protect.php');
+    // require_once('protect.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,14 +9,23 @@
     <title>home</title>
     <link rel="stylesheet" href="Assets/style-CSS/style_home.css">
 <body>
+    <header>
+        <div id="cabecalho">
+            <p>
+                MÜRAL
+            </p>
+        </div>
+        <div>
+            <button><a href="Perfil/index.php"><img src="Assets/imagens/Olho_aberto.png" alt="foto_de_perfil"></a></button>            
+        </div>
+        <div id="search_box">
+            <input type="text" placeholder="Pesquisar..." id="search_input">
+            <img src="Assets/imagens/search.png" alt="" id="search_png">
+        </div>
+    </header>
     <div>
         <p>
             <a href="logout.php">Sair</a>
-        </p>        
-    </div>
-    <div>
-        <p>
-            <button><a href="Perfil/index.php"><img src="Assets/imagens/Olho_aberto.png" alt="foto_de_perfil"></a></button>
         </p>        
     </div>
     <div>
