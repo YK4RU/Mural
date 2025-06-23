ad8fe75183d08f11453f270981ac7833057cca9d
YK4RU <168197162+YK4RU@users.noreply.github.com>
2025-06-23 01:35:15 -0300

    Add files via upload

--- Mural_projeto/Mural/Perfil/alterar_perfil.php
+++ Mural_projeto/Mural/Perfil/alterar_perfil.php
@@ -2,63 +2,93 @@
 require_once("../conexao.php");
 require_once('../protect.php');
 
-$sql_select = $conexao->prepare("SELECT nome, descricao, data_nasc FROM usuario WHERE email = :email");
+$sql_select = $conexao->prepare("SELECT url_da_imagem_do_perfil, url_da_imagem_do_banner, nome, descricao, data_nasc FROM usuario WHERE email = :email");
 $sql_select->bindValue(':email', $_SESSION['email']);
 $sql_select->execute();
-            
+
 if($sql_select->rowCount() > 0) {
     $dados = $sql_select->fetch();
 }     
+
+$sql_select_img = $conexao->prepare("SELECT url_da_imagem_do_perfil, url_da_imagem_do_banner FROM usuario WHERE email = :email");
+$sql_select_img->bindValue(':email', $_SESSION['email']);
+$sql_select_img->execute();
+$imagem = $sql_select_img->fetch();
+            
 ?>
 
 
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <title>Alterar perfil</title>
-</head>
-<body>
-    <form action="" method="POST" enctype="multipart/form-data">
-        <?php
-        include_once("config_alterar_perfil.php"); 
-        ?>
-        <p>
-            <label>Alterar foto de perfil</label>
-        </p>
-        <p>
-            <input type="file" name="foto_perfil" id="foto_perfil" accept="image/*">
-        </p>
-        <p>
-            <label>Alterar banner</label>
-        </p>
-        <p>
-            <input type="file" name="banner" id="banner" accept="image/*">
-        </p>
-        <p>
-            <label>Alterar nome</label>
-        </p>
-        <p>
-            <input type="text" name="nome_perfil" id="nome_perfil" value="<?php echo $dados['nome'];?>">
-        </p>
-        <p>
-            <label>Adicionar uma descrição</label>
-        </p>
-        <p>
-            <input type="text" name="descricao_perfil" id="descricao_perfil" value="<?php echo $dados['descricao'];?>">
-        </p>
-        <p>
-            <label>Alterar data de nascimento</label>
-        </p>
-        <p>
-            <input type="date" name="data_nasc" id="data_nasc" value="<?php echo $dados['data_nasc'];?>">
-        </p>
-        <p>
-            <input type="submit" value="Salvar alterações">
-        </p>
-        <p>
-            <button><a href="config_deletar_perfil.php">Deletar perfil</a></button>
-        </p>
-    </form>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Perfil</title>
+        <link rel="stylesheet" href="../Assets/style-CSS/style_alt_perfil.css">
+    </head>
+    <body>
+        <div id="formulario">
+            <form action="" method="POST" enctype="multipart/form-data">
+                <?php
+                include_once("config_alterar_perfil.php"); 
+                ?>
+
+                <div id="imagem_banner">
+                    <?php if (!empty($imagem['url_da_imagem_do_banner'])): ?>
+                        <img src="../<?php echo $imagem['url_da_imagem_do_banner']; ?>" alt="Banner atual">
+                    <?php endif; ?>
+                    <p id="banner_txt">
+                        Alterar banner
+                    </p>
+                    <p>
+                        <input type="file" name="banner" id="banner" accept="image/*">
+                    </p>
+                </div>
+                <div id="imagem_perfil">
+                    <?php if (!empty($imagem['url_da_imagem_do_perfil'])): ?>
+                        <img src="../<?php echo $imagem['url_da_imagem_do_perfil']; ?>" alt="Foto de perfil atual">
+                    <?php endif; ?>
+                    <p id="perfil_txt">
+                        Alterar imagem de perfil
+                    </p>
+                    <p>
+                        <input type="file" name="foto_perfil" id="foto_perfil" accept="image/*">
+                    </p>
+                </div>
+
+
+                <div>
+                    <p>
+                        <label for="nome_perfil" id="nome">Alterar nome</label>
+                    </p>
+                    <p>
+                        <input type="text" class="input" name="nome_perfil" id="nome_perfil" value="<?php echo $dados['nome'];?>">
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        <label for="descricao_perfil" id="desc_perfil">Adicionar uma descrição</label>
+                    </p>
+                    <p>
+                        <input type="text"  class="input" name="descricao_perfil" id="descricao_perfil" value="<?php echo $dados['descricao'];?>">
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        <label for="data_nasc" id="data_perfil">Alterar data de nascimento</label>
+                    </p>
+                    <p>
+                        <input type="date" class="input" name="data_nasc" id="data_nasc" value="<?php echo $dados['data_nasc'];?>">
+                    </p>
+                </div>
+            
+                <p>
+                    <input type="submit" value="Salvar alterações" id="botao_salvar">
+                </p>
+                <p>
+                    <button><a href="config_deletar_perfil.php" id="botao_deletar">Deletar perfil</a></button>
+                </p>
+            </form>
+        </div>
+    </body>
 </html>
--- Mural_projeto/Mural/Perfil/index.php
+++ Mural_projeto/Mural/Perfil/index.php
@@ -1,23 +1,111 @@
+<?php
+require_once("../conexao.php");
+require_once('../protect.php');
+
+$sql_select = $conexao->prepare("SELECT url_da_imagem_do_perfil, url_da_imagem_do_banner, nome, descricao, nome_de_usuario FROM usuario WHERE email = :email");
+$sql_select->bindValue(':email', $_SESSION['email']);
+$sql_select->execute();
+
+if($sql_select->rowCount() > 0) {
+    $dados = $sql_select->fetch();
+}     
+
+$sql_select_img = $conexao->prepare("SELECT url_da_imagem_do_perfil, url_da_imagem_do_banner FROM usuario WHERE email = :email");
+$sql_select_img->bindValue(':email', $_SESSION['email']);
+$sql_select_img->execute();
+$imagem = $sql_select_img->fetch();
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Perfil</title>
-    <link rel="stylesheet" href="Assets/style-CSS/style_perfil.css">
-</head>
-<body>
-    <?php
-        require_once("config_perfil.php"); 
-    ?>
-    <div class="container">
-        <a href="alterar_perfil.php">a</a>
-        <figure>
-            <img src="../Assets/imagens/uploads/banner/banner_defaut.png" alt="banner" id="imagem_banner">
-        </figure>
-        <figure>
-            <img src="../Assets/imagens/uploads/perfil/perfil_default.png" alt="perfil" id="imagem_perfil">
-        </figure>
-    </div>
-</body>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Perfil</title>
+        <link rel="stylesheet" href="../Assets/style-CSS/style_perfil.css">
+    </head>
+    <body>
+        <?php
+            require_once("config_perfil.php"); 
+        ?>
+        <header>
+            <div id="banner_perfil_foto">
+                <div id="imagem_banner"> 
+                    <?php if (!empty($imagem['url_da_imagem_do_banner'])): ?>
+                        <img src="../<?php echo $imagem['url_da_imagem_do_banner']; ?>" alt="Foto de banner atual">
+                    <?php endif; ?>
+                </div>
+                <div id="imagem_perfil"> 
+                    <?php if (!empty($imagem['url_da_imagem_do_perfil'])): ?>
+                        <img src="../<?php echo $imagem['url_da_imagem_do_perfil']; ?>" alt="Foto de perfil atual">
+                    <?php endif; ?>
+                </div>
+            </div>
+                <div id="edit_perfil"> <a href="alterar_perfil.php">Editar perfil</a></div>
+                <div id="desc_perfil"> <p><?php echo $dados['descricao'];?></p></div>
+                <div id="nome_perfil"> <p><?php echo $dados['nome'];?></p></div>
+                <div id="user"> <p>@<?php echo $dados['nome_de_usuario'];?></p></div>
+        </header>
+
+        <!-- postagens -->
+        <section>
+            <div></div>
+        </section>
+
+        <aside id="barra_lateral">
+            <div id="titulo">
+                <p>
+                    MÜRAL
+                </p>
+            </div>
+            <div class="botao_barra_lateral">
+                <p>
+                    <a href="../home_page.php" class="botao_barra_lateral_A"><img src="../Assets/imagens/casa.png" alt=""></a>
+                </p>        
+            </div>
+            <div class="botao_barra_lateral">
+                <p>
+                    <a href="../Search/index.php" class="botao_barra_lateral_A"><img src="../Assets/imagens/search.png" alt=""></a>
+                </p>        
+            </div>
+            <div class="botao_barra_lateral">
+                <p>
+                    <a href="../Mensagem/index.php" class="botao_barra_lateral_A"><img src="../Assets/imagens/mensagem.png" alt=""></a>
+                </p>        
+            </div>
+            <div class="botao_barra_lateral">
+                <p>
+                    <a href="../Notificacao/index.php" class="botao_barra_lateral_A"><img src="../Assets/imagens/sino_notificacao.png" alt=""></a>
+                </p>        
+            </div>
+            <div class="botao_barra_lateral">
+                <p>
+                    <a href="../Perfil/index.php" class="botao_barra_lateral_A"><img src="../Assets/imagens/perfil.png" alt=""></a>
+                </p>        
+            </div>
+            
+            <div id="sair_A">
+                <a href="../logout.php">Sair</a>       
+            </div>
+            <div id="florzinha">
+                <img src="../Assets/imagens/flor.png" alt="">
+            </div>
+        </aside>
+
+        <aside id="outra_barra_lateral">
+            <div id="mural_links">
+                <a href="../Mural/index.php">Müral</a>
+            </div>
+            <div id="pasta_links">
+                <a href="../Pastas/index.php">Pastas</a>
+            </div>
+            
+            <div id="botao_criar_postagem">
+                <img src="../Assets/imagens/botao_criar.png" alt="">
+                <a href="../Postagem/index.php">Criar</a>
+            </div>
+        </aside>
+
+    </body>
 </html>
