c9db1fd67aac853b3086d4b330a4560d7b77cba5
YK4RU <168197162+YK4RU@users.noreply.github.com>
2025-06-25 08:25:44 -0300

    Add files via upload

--- Mural_projeto/Mural/Cadastro/config_cadastro.php
+++ Mural_projeto/Mural/Cadastro/config_cadastro.php
@@ -11,31 +11,42 @@ if (isset($_POST["email"]) && isset($_POST["senha"]) && isset($_POST["nome_de_us
     $nome_de_usuario = trim($_POST["nome_de_usuario"]);
 
     if (empty($email) || empty($senha) || empty($nome_de_usuario)) {
-        echo "Preencha todos os campos";
-    }
+        echo '<p class="mensagem_erro">Preencha todos os campos.</p>';
+    } 
     else {
 
-        $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
+        $sql_verifica = $conexao->prepare("SELECT * FROM usuario WHERE email = :email OR nome_de_usuario = :nome_usuario");
+        $sql_verifica->bindValue(':email', $email);
+        $sql_verifica->bindValue(':nome_usuario', $nome_de_usuario);
+        $sql_verifica->execute();
 
-        $sql_code = $conexao->prepare("INSERT INTO usuario (email, senha, nome_de_usuario) VALUES (:email, :senha, :nome_usuario )");
-        $sql_code->bindValue(':email', $email);
-        $sql_code->bindValue(':senha', $senha_hash);
-        $sql_code->bindValue(':nome_usuario', $nome_de_usuario);
-        $sql_code->execute();
-
-        if ($sql_code->rowCount() > 0) {
-            $sql_select = $conexao->prepare("SELECT * FROM usuario WHERE email = :email");
-            $sql_select->bindValue(':email', $email);
-            $sql_select->execute();
-            $usuario = $sql_select->fetch();
-
-            $_SESSION['email'] = $usuario['email'];
-            $_SESSION['id_usuario'] = $usuario['id_usuario'];
-            $_SESSION['nome_de_usuario'] = $usuario['nome_de_usuario'];
-            header("location: ../home_page.php");
-            exit;
+        if ($sql_verifica->rowCount() > 0) {
+            echo '<p class="mensagem_erro">E-mail ou nome de usuário já cadastrado.</p>';
         }
+        else {
+
+            $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
+
+            $sql_code = $conexao->prepare("INSERT INTO usuario (email, senha, nome_de_usuario) VALUES (:email, :senha, :nome_usuario )");
+            $sql_code->bindValue(':email', $email);
+            $sql_code->bindValue(':senha', $senha_hash);
+            $sql_code->bindValue(':nome_usuario', $nome_de_usuario);
+            $sql_code->execute();
+
+            if ($sql_code->rowCount() > 0) {
+                $sql_select = $conexao->prepare("SELECT * FROM usuario WHERE email = :email");
+                $sql_select->bindValue(':email', $email);
+                $sql_select->execute();
+                $usuario = $sql_select->fetch();
+
+                $_SESSION['email'] = $usuario['email'];
+                $_SESSION['id_usuario'] = $usuario['id_usuario'];
+                $_SESSION['nome_de_usuario'] = $usuario['nome_de_usuario'];
+                header("location: ../home_page.php");
+                exit;
+            }
     
-    } 
+        } 
+    }
 }
 ?>
